085a12b6e1ed390453d797001e6039c73a2cab8e
real-codemaster <saxenaakanshaa@gmail.com>
2023-07-06 13:47:57 +0530

    added cart

---
+++ Payment.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Payment</title>
+    <link rel="stylesheet" href="Footer.css" />
+    <script
+      src="https://kit.fontawesome.com/1bf9f84d4d.js"
+      crossorigin="anonymous"
+    ></script>
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap");
+
+      body {
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0;
+        padding: 0;
+      }
+
+      html {
+        box-sizing: border-box;
+      }
+
+      *,
+      *:before,
+      *:after {
+        box-sizing: inherit;
+      }
+      .box {
+        width: 100%;
+        /* margin-bottom:20px; */
+        margin: auto;
+        padding: 30px;
+        background: #f1f3f6;
+      }
+      .card {
+        box-shadow: 0 5px 9px 0 rgba(0, 0, 0, 0.2);
+        margin: 20px 70px;
+        background-color: white;
+      }
+
+      .container {
+        padding: 15px 30px;
+        text-align: justify;
+      }
+      /* .container::after{
+      content: "";
+       display:table;
+
+      } */
+
+      @media screen and (max-width: 273px) {
+        .box {
+          width: 100%;
+          display: block;
+        }
+      }
+    </style>
+  </head>
+  <body>
+    <div class="bgcolour-change">
+      <div class="box">
+        <div class="card">
+          <div class="container">
+            <h2
+              style="
+                text-align: center;
+                font-family: 'Times New Roman', Times, serif;
+              "
+            >Payments
+            </h2>
+            <h4>How do I pay for a SHOP BUDDY purchase?</h4>
+            <p>SHOP BUDDY offers you multiple payment methods. Whatever your online mode of payment, you can rest assured that SHOP BUDDY's trusted payment gateway partners use secure encryption technology to keep your transaction details confidential at all times.</p>
+            <p>You may use Internet Banking, Gift Card, Cash on Delivery and Wallet to make your purchase.</p>
+
+            <h4>Are there any hidden charges (Octroi or Sales Tax) when I make a purchase on SHOP BUDDY?</h4>
+            <p>There are NO hidden charges when you make a purchase on SHOP BUDDY. The prices listed for all the items are final and all-inclusive. The price you see on the product page is exactly what you pay.</p>
+            <p>Delivery charges may be extra depending on the seller policy. Please check individual seller for the same. In case of seller WS Retail, the ₹50 delivery charge is waived off on orders worth ₹500 and over.</p>
+
+            <h4>What is Cash on Delivery?</h4>
+            <p>If you are not comfortable making an online payment on SHOP BUDDY.com, you can opt for the Cash on Delivery (C-o-D) payment method instead. With C-o-D you can pay in cash at the time of actual delivery of the product at your doorstep, without requiring you to make any advance payment online.</p>
+            <p>The maximum order value for a Cash on Delivery (C-o-D) payment is ₹50,000. It is strictly a cash-only payment method. Gift Cards or store credit cannot be used for C-o-D orders. Foreign currency cannot be used to make a C-o-D payment. Only Indian Rupees accepted.</p>
+
+            <h4>How do I pay using a credit/debit card?</h4>
+            <p>We accept payments made by credit/debit cards issued in India and 21 other countries.</p>
+            <h5>Credit cards</h5>
+            <p>We accept payments made using Visa, MasterCard and American Express credit cards.</p>
+            <p>To pay using your credit card at checkout, you will need your card number, expiry date, three-digit CVV number (found on the backside of your card). After entering these details, you will be redirected to the bank's page for entering the online 3D Secure password.</p>
+            <h5>Debit cards</h5>
+            <p>We accept payments made using Visa, MasterCard and Maestro debit cards.</p>
+            <p>To pay using your debit card at checkout, you will need your card number, expiry date (optional for Maestro cards), three-digit CVV number (optional for Maestro cards). You will then be redirected to your bank's secure page for entering your online password (issued by your bank) to complete the payment.</p>
+
+            <h4>Is it safe to use my credit/debit card on SHOP BUDDY?</h4>
+            <p>Your online transaction on SHOP BUDDY is secure with the highest levels of transaction security currently available on the Internet. SHOP BUDDY uses 256-bit encryption technology to protect your card information while securely transmitting it to the respective banks for payment processing.</p>
+            <p>All credit card and debit card payments on SHOP BUDDY are processed through secure and trusted payment gateways managed by leading banks. Banks now use the 3D Secure password service for online transactions, providing an additional layer of security through identity verification.</p>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+    
+  </body>
+</html>
+<?php require('footer.inc.php')?>
