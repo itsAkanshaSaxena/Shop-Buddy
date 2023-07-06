7b85424691d8b5162607eee49d1d87bef689f1f9
real-codemaster <saxenaakanshaa@gmail.com>
2023-07-06 13:54:06 +0530

    added terms and condition page

---
+++ termsandcondition.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Tearms and conditions</title>
+
+    <link rel="stylesheet" href="Footer.css" />
+    <script
+      src="https://kit.fontawesome.com/1bf9f84d4d.js"
+      crossorigin="anonymous"
+    ></script>
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap");
+
+      body {
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0;
+        padding: 0;
+      }
+
+      html {
+        box-sizing: border-box;
+      }
+      *,
+      *:before,
+      *:after {
+        box-sizing: inherit;
+      }
+      .box {
+        width: 100%;
+        /* margin-bottom:20px; */
+        margin: auto;
+        padding: 30px;
+        background: #f1f3f6;
+      }
+      .card {
+        box-shadow: 0 5px 9px 0 rgba(0, 0, 0, 0.2);
+        margin: 20px 70px;
+        background-color: white;
+      }
+
+      .container {
+        padding: 15px 30px;
+        text-align: justify;
+      }
+      /* .container::after{
+      content: "";
+       display:table;
+
+      } */
+
+      @media screen and (max-width: 273px) {
+        .box {
+          width: 100%;
+          display: block;
+        }
+      }
+    </style>
+  </head>
+  <body>
+    <div class="bgcolour-change">
+      <div class="box">
+        <div class="card">
+          <div class="container">
+            <h2
+              style="
+                text-align: center;
+                font-family: 'Times New Roman', Times, serif;
+              "
+            >
+              SHOP BUDDY Terms of use
+            </h2>
+            <p>
+              The project belongs to SHOP BUDDY, a group of four final year
+              students pursuing B.C.A. from Banasthali Vidyapith.
+            </p>
+            <p>
+              Your use of the Platform and services and tools are governed by
+              the following terms and conditions ("Terms of Use") as applicable
+              to the Platform including the applicable policies which are
+              incorporated herein by way of reference. If You transact on the
+              Platform, You shall be subject to the policies that are applicable
+              to the Platform for such transaction. By mere use of the Platform,
+              You shall be contracting with SHOP BUDDY Internet Private Limited
+              and these terms and conditions including the policies constitute
+              Your binding obligations, with SHOP BUDDY.
+            </p>
+            <p>
+              For the purpose of these Terms of Use, wherever the context so
+              requires "You" or "User" shall mean any natural or legal person
+              who has agreed to become a buyer on the Platform by providing
+              Registration Data while registering on the Platform as Registered
+              User using the computer systems. SHOP BUDDY allows the User to
+              surf the Platform or making purchases without registering on the
+              Platform. The term "We", "Us", "Our" shall mean SHOP BUDDY
+              Internet Private Limited.
+            </p>
+            <p>
+              When You use any of the services provided by Us through the
+              Platform, including but not limited to, (e.g. Product Reviews,
+              Seller Reviews), You will be subject to the rules, guidelines,
+              policies, terms, and conditions applicable to such service, and
+              they shall be deemed to be incorporated into this Terms of Use and
+              shall be considered as part and parcel of this Terms of Use. We
+              reserve the right, at Our sole discretion, to change, modify, add
+              or remove portions of these Terms of Use, at any time without any
+              prior written notice to You. It is Your responsibility to review
+              these Terms of Use periodically for updates / changes. Your
+              continued use of the Platform following the posting of changes
+              will mean that You accept and agree to the revisions. As long as
+              You comply with these Terms of Use, We grant You a personal,
+              non-exclusive, non-transferable, limited privilege to enter and
+              use the Platform.
+            </p>
+            <h4>Your Account and Registration Obligations</h4>
+            <p>
+              If You use the Platform, You shall be responsible for maintaining
+              the confidentiality of your Display Name and Password and You
+              shall be responsible for all activities that occur under your
+              Display Name and Password. You agree that if You provide any
+              information that is untrue, inaccurate, not current or incomplete
+              or We have reasonable grounds to suspect that such information is
+              untrue, inaccurate, not current or incomplete, or not in
+              accordance with the this Terms of Use, We shall have the right to
+              indefinitely suspend or terminate or block access of your
+              membership on the Platform and refuse to provide You with access
+              to the Platform.
+            </p>
+            <p>
+              Your mobile phone number and/or e-mail address is treated as Your
+              primary identifier on the Platform. It is your responsibility to
+              ensure that Your mobile phone number and your email address is up
+              to date on SHOP BUDDYform at all times. You agree to notify Us
+              promptly if your mobile phone number or e-mail address changes by
+              updating the same on the Platform through a onetime password
+              verification.
+            </p>
+            <p>
+              You agree that SHOP BUDDY shall not be liable or responsible for
+              the activities or consequences of use or misuse of any information
+              that occurs under your Account in cases, including, where You have
+              failed to update Your revised mobile phone number and/or e-mail
+              address on the Website Platform.
+            </p>
+            <p>
+              If You share or allow others to have access to Your account on the
+              Platform (“Account”), by creating separate profiles under Your
+              Account, or otherwise, they will be able to view and access Your
+              Account information. You shall be solely liable and responsible
+              for all the activities undertaken under Your Account, and any
+              consequences therefrom.
+            </p>
+            <h4>Platform for Transaction and Communication</h4>
+            <ol>
+              <li>
+                <p>
+                  SHOP BUDDY does not make any representation or Warranty as to
+                  specifics (such as quality, value, salability, etc) of the
+                  products or services proposed to be sold or offered to be sold
+                  or purchased on the Platform. SHOP BUDDY does not implicitly
+                  or explicitly support or endorse the sale or purchase of any
+                  products or services on the Platform. SHOP BUDDY accepts no
+                  liability for any errors or omissions, whether on behalf of
+                  itself or third parties.
+                </p>
+              </li>
+              <li>
+                <p>
+                  SHOP BUDDY is not responsible for any non-performance or
+                  breach of any contract entered into between Buyers and
+                  SeSHOP BUDDY cannot and does not guarantee that the
+                  concerned Buyers and/or Sellers will perform any transaction
+                  concluded on the Platform. SHOP BUDDY shall not and is not
+                  required to mediate or resolve any dispute or disagreement
+                  between Buyers and Sellers.
+                </p>
+              </li>
+              <li>
+                <p>
+                  SHOP BUDDY does not make any representation or warranty as to
+                  the item-specifics (such as legal title, creditworthiness,
+                  identity, etc) of any of its Users. You are advised to
+                  independently verify the bona fides of any particular User
+                  that You choose to deal with on the Platform and use Your best
+                  judgment in that behalf.
+                </p>
+              </li>
+              <li>
+                <p>
+                  SHOP BUDDY does not at any point of time during any
+                  transaction between Buyer and Seller on the Platform come into
+                  or take possession of any of the products or services offered
+                  by Seller nor does it at any point gain title to or have any
+                  rights or claims over the products or services offered by
+                  Seller to Buyer.
+                </p>
+              </li>
+              <li>
+                <p>
+                  At no time shall SHOP BUDDY hold any right, title or interest
+                  over the products nor shall SHOP BUDDY have any obligations or
+                  liabilities in respect of such contract entered into between
+                  Buyers and Sellers. SHOP BUDDY is not responsible for
+                  unsatisfactory or delayed performance of services or damages
+                  or delays as a result of products which are out of stock,
+                  unavailable or back ordered.
+                </p>
+              </li>
+              <li>
+                <p>
+                  The Platform is only a platform that can be utilized by Users
+                  to reach a larger base to buy and sell products or services.
+                  SHOP BUDDY is only providing a platform for communication and it
+                  is agreed that the contract for sale of any of the products or
+                  services shall be a strictly bipartite contract between the
+                  Seller and the Buyer.
+                </p>
+              </li>
+            </ol>
+
+            <p>
+              At no time shall SHOP BUDDY hold any right, title or interest over
+              the products nor shall SHOP BUDDY have any obligations or
+              liabilities in respect of such contract.
+            </p>
+            <p>
+              SHOP BUDDY is not responsible for unsatisfactory or delayed
+              performance of services or damages or delays as a result of
+              products which are out of stock, unavailable or back ordered.
+            </p>
+            <h3>Charges</h3>
+            <p>
+              Membership on the Platform is free for buyers. SHOP BUDDY does not
+              charge any fee for browsing and buying on the Platform. SHOP BUDDY
+              reserves the right to change its Fee Policy from time to time. In
+              particular, SHOP BUDDY may at its sole discretion introduce new
+              services and modify some or all of the existing services offered
+              on the Platform. In such an event SHOP BUDDY reserves the right to
+              introduce fees for the new services offered or amend/introduce
+              fees for existing services, as the case may be. Changes to the Fee
+              Policy shall be posted on the Platform and such changes shall
+              automatically become effective immediately after they are posted
+              on the Platform. Unless otherwise stated, all fees shall be quoted
+              in Indian Rupees. You shall be solely responsible for compliance
+              of all applicable laws including those in India for making
+              payments to SHOP BUDDY Internet Private Limited.
+            </p>
+
+            <h3>Use of the Platform</h3>
+            <p>
+              You agree, undertake and confirm that Your use of Platform shall
+              be strictly governed by the following binding principles:<br />
+
+              You shall not host, display, upload, modify, publish, transmit,
+              update or share any information which:
+            </p>
+            <ol type="i">
+              <li>
+                <p>
+                  belongs to another person and to which You does not have any
+                  right to;
+                </p>
+              </li>
+              <li>
+                <p>
+                  is grossly harmful, harassing, blasphemous, defamatory,
+                  obscene, pornographic, paedophilic, libellous, invasive of
+                  another's privacy, hateful, or racially, ethnically
+                  objectionable, disparaging, relating or encouraging money
+                  laundering or gambling, or otherwise unlawful in any manner
+                  whatever; or unlawfully threatening or unlawfully harassing
+                  including but not limited to "indecent representation of
+                  women" within the meaning of the Indecent Representation of
+                  Women (Prohibition) Act, 1986;
+                </p>
+              </li>
+              <li><p>is misleading in any way;</p></li>
+              <li>
+                <p>
+                  is patently offensive to the online community, such as
+                  sexually explicit content, or content that promotes obscenity,
+                  paedophilia, racism, bigotry, hatred or physical harm of any
+                  kind against any group or individual;
+                </p>
+              </li>
+              <li>
+                <p>harasses or advocates harassment of another person;</p>
+              </li>
+              <li>
+                <p>
+                  promotes illegal activities or conduct that is abusive,
+                  threatening, obscene, defamatory or libellous;
+                </p>
+              </li>
+              <li>
+                <p>
+                  tries to gain unauthorized access or exceeds the scope of
+                  authorized access to the Platform or to profiles, blogs,
+                  communities, account information, bulletins, friend request,
+                  or other areas of the Platform or solicits passwords or
+                  personal identifying information for commercial or unlawful
+                  purposes from other users;
+                </p>
+              </li>
+              <li>
+                <p>
+                  contains software viruses or any other computer code, files or
+                  programs designed to interrupt, destroy or limit the
+                  functionality of any computer resource; or contains any trojan
+                  horses, worms, time bombs, cancelbots, easter eggs or other
+                  computer programming routines that may damage, detrimentally
+                  interfere with, diminish value of, surreptitiously intercept
+                  or expropriate any system, data or personal information;
+                </p>
+              </li>
+              <li>
+                <p>
+                  threatens the unity, integrity, defence, security or
+                  sovereignty of India, friendly relations with foreign states,
+                  or public order or causes incitement to the commission of any
+                  cognizable offence or prevents investigation of any offence or
+                  is insulting any other nation.
+                </p>
+              </li>
+              <li><p>shall not be false, inaccurate or misleading;</p></li>
+              <li><p>harm minors in any way;</p></li>
+              <li><p>violates any law for the time being in force;</p></li>
+            </ol>
+
+            <p>
+              In addition, We can (and You hereby expressly authorize Us to)
+              disclose any information about You to law enforcement or other
+              government officials, as we, in Our sole discretion, believe
+              necessary or appropriate in connection with the investigation
+              and/or resolution of possible crimes, especially those that may
+              involve personal injury.
+            </p>
+            <p>
+              We reserve the right, but has no obligation, to monitor the
+              materials posted on the Platform. SHOP BUDDY shall have the right to
+              remove or edit any content that in its sole discretion violates,
+              or is alleged to violate, any applicable law or either the spirit
+              or letter of these Terms of Use. Notwithstanding this right, YOU
+              REMAIN SOLELY RESPONSIBLE FOR THE CONTENT OF THE MATERIALS YOU
+              POST ON THE PLATFORM AND IN YOUR PRIVATE MESSAGES.
+            </p>
+            <p>
+              SHOP BUDDY shall have all the rights to take necessary action and
+              claim damages that may occur due to your involvement/participation
+              in any way on your own or through group/s of people, intentionally
+              or unintentionally in DoS/DDoS (Distributed Denial of Services).
+            </p>
+            <p>
+              It is possible that other users (including unauthorized users or
+              "hackers") may post or transmit offensive or obscene materials on
+              the Platform and that You may be involuntarily exposed to such
+              offensive and obscene materials. It also is possible for others to
+              obtain personal information about You due to your use of the
+              Platform, and that the recipient may use such information to
+              harass or injure You. We does not approve of such unauthorized
+              uses, but by using the Platform You acknowledge and agree that We
+              are not responsible for the use of any personal information that
+              You publicly disclose or share with others on the Platform. Please
+              carefully select the type of information that You publicly
+              disclose or share with others on the Platform SHOP BUDDY.
+
+             
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    
+  </body>
+</html>
+<?php require('footer.inc.php')?>
