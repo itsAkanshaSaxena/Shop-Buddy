7af08cca67b32d26520742f79d978b78423bd756
real-codemaster <saxenaakanshaa@gmail.com>
2023-07-06 13:52:26 +0530

    added faq page

---
+++ faq.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>FAQ</title>
+    <link rel="stylesheet" href="Footer.css" />
+    <script
+      src="https://kit.fontawesome.com/1bf9f84d4d.js"
+      crossorigin="anonymous"
+    ></script>
+    <style>
+      @import url("https://fonts.googleapis.com/css2?family=Ubuntu:wght@300&display=swap");
+
+      body {
+        font-family: Arial, Helvetica, sans-serif;
+        margin: 0;
+        padding: 0;
+      }
+
+      html {
+        box-sizing: border-box;
+      }
+
+      *,
+      *:before,
+      *:after {
+        box-sizing: inherit;
+      }
+      .box {
+        width: 100%;
+        /* margin-bottom:20px; */
+        margin: auto;
+        padding: 30px;
+        background: #f1f3f6;
+      }
+      .card {
+        box-shadow: 0 5px 9px 0 rgba(0, 0, 0, 0.2);
+        margin: 20px 70px;
+        background-color: white;
+      }
+
+      .container {
+        padding: 15px 30px;
+        text-align: justify;
+      }
+      /* .container::after{
+      content: "";
+       display:table;
+
+      } */
+
+      @media screen and (max-width: 273px) {
+        .box {
+          width: 100%;
+          display: block;
+        }
+      }
+    </style>
+  </head>
+  <body>
+    <div class="bgcolour-change">
+      <div class="box">
+        <div class="card">
+          <div class="container">
+            <h2
+              style="
+                text-align: center;
+                font-family: 'Times New Roman', Times, serif;
+              "
+            >
+             FAQs
+            </h2>
+            <h3>Is it necessary to have an account to shop on SHOP BUDDY?</h3>
+            <p>Yes, it's necessary to log into your SHOP BUDDY account to shop. Shopping as a logged-in user is fast & convenient and also provides extra security.
+<br>
+                You'll have access to a personalised shopping experience including recommendations and quicker check-out.</p>
+            <h3>Do sellers on SHOP BUDDY ship internationally?</h3>
+            <p>Currently, sellers on SHOP BUDDY only ship within India.</p>
+            <h3>What can I do if the balance in my wallet is not enough to pay for my order?</h3>
+            <p>You can pay part of the amount through your wallet and the remaining through any other prepaid payment modes like Credit/Debit Cards, and Netbanking.
+<br>
+                Please note that wallet cannot be combined with Cash/Card on Delivery (CoD) payment mode for now, and bank offers will not applicable for partial payments from the wallet.</p>
+            <h3>What are the standard shipping speeds and delivery charges?</h3>
+            <p>Standard Shipping Speeds and Delivery charges  vary based on the destination pincode entered.
+            </p>
+            <h3>Why can't I get some items shipped to my location?</h3>
+            <p>Some sellers prefer not to ship to certain locations due to the cost involved and this is entirely at their discretion. You can check with your pincode on the product page to see any other sellers ship to your location.</p>
+            <h3>Why are faster delivery options not available at my location?</h3>
+            <p>Faster delivery options may not be available at your location due to courier related reasons but we’re trying to expand our services to include more places. Stay tuned for updates!</p>
+            <h3>Do I need to verify my mobile number or email address every time I log in?</h3>
+            <p>As the verification step is a one time process, you won't have to do it again once your account is verified.</p>
+            <h3>Why do I need to verify my mobile number or email address to log into my SHOP BUDDY account?</h3>
+            <p>To make sure that your account details are always secure, verification by OTP (One Time Password) is important. This is a one-time process and you can log into your Flipkart account without any hassles once this is done.</p>
+            <h3>How can I claim warranty for my product?</h3>
+            <p>You can claim the warranty for your product by using the invoice at any authorized service center of the brand.</p>
+            <h3>Can I order a product that is 'Out of Stock' or 'Temporarily Unavailable'?</h3>
+            <p>The products listed as 'Out of Stock' or 'Temporarily Unavailable' are not available for sale. Please use the 'Notify Me' feature to be informed of the product's availability with sellers on Flipkart.</p>
+            <h3>What does 'Out of Stock' mean?</h3>
+            <p>An item is marked as 'Out of stock' when it is not available with any sellers at the moment; you won't be able to buy it now. Use 'Notify Me' feature to know once it's available with sellers for purchase.</p>
+            <h3>Can I pay with any Credit card?</h3>
+            <p>You can choose to pay on SHOP BUDDY with any Visa, MasterCard or American Express credit card issued in India.</p>
+            <h3>How can I pay for my order?</h3>
+            <p>You can choose to pay for an order using any of the below methods:</p>
+            <ol>
+                <li><p>Cash on Delivery</p></li>
+                <li><p>Visa, MasterCard, Maestro and American Express Credit or Debit cards issued in India and 21 other countries</p></li>
+                <li><p>Net Banking</p></li>
+               
+            </ol>
+     
+            <h3></h3>
+            <p></p>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+
+
+
+
+    
+  </body>
+</html>
+<?php require('footer.inc.php')?>
